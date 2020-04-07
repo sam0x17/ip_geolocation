acb47a2463c13980e42eaa4c2b191c86d427104f
Sam Johnson <sam@durosoft.com>
2020-04-07 03:15:47 -0400

    attempt to rename everything

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,6 @@
 require "spec"
-require "../src/location"
-require "../src/lookup"
-require "../src/ip_address"
+require "../src/ip_geolocation"
+
+alias Lookup = IPGeolocation::Lookup
+alias Location = IPGeolocation::Location
+alias IPAddress = IPGeolocation::IPAddress
