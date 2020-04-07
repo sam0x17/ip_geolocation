d57540325ebd760eb634f7e4d6c3bbbe3dc5689d
Sam Johnson <sam@durosoft.com>
2020-04-07 03:20:13 -0400

    all specs passing

--- src/lookup.cr
+++ src/lookup.cr
@@ -5,9 +5,9 @@ module IPGeolocation
   class Lookup
 
     DEFAULT_INPUT_PATH = "data/IP2LOCATION-LITE-DB3.zip"
-    @mapping = Hash(Range(UInt32, UInt32), UInt32).new
+    @mapping = Hash(Range(UInt32, UInt32), UInt64).new
     @keys = Array(Range(UInt32, UInt32)).new
-    @locations = Hash(UInt32, Location).new
+    @locations = Hash(UInt64, Location).new
 
     delegate :size, to: @mapping
 
@@ -38,13 +38,12 @@ module IPGeolocation
 
         parsed_csv.each do |row|
           location = Location.new(row[2], row[3], row[4], row[5])
-          digest = location.hash.to_u32
+          digest = location.hash
           @locations[digest] = location
           @mapping[row[0].to_u32..row[1].to_u32] = digest
         end
 
         indexed_records_count += slice.size
-        puts "#{indexed_records_count} records indexed"
       end
       @keys = @mapping.keys
     end
